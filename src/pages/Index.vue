1c8ce562a2c02d4ac4dccb8a47d487997435588b
Helen Evans <helenijevans@gmail.com>
2020-10-20 14:06:26 +0100

    starting form

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,13 +9,8 @@
                 {{states_dictionary[book.status]}}
               </p>
             </span>
-            <span v-else-if="book.status == 2">
+            <span v-else>
               <p class="absolute-top-right defau-label notavai-label">
-                {{states_dictionary[book.status]}}
-              </p>
-            </span>
-             <span v-else>
-              <p class="absolute-top-right defau-label">
                 {{states_dictionary[book.status]}}
               </p>
             </span>
@@ -27,15 +22,61 @@
           <div class="text-subtitle2">{{book.author}}</div>
         </q-card-section>
 
-        <q-card-section class="q-pt-none">
-         {{}}
-        </q-card-section>
-
         <q-card-actions>
-          <q-btn flat>Action 1</q-btn>
-          <q-btn flat>Action 2</q-btn>
+          <div v-if="book.status == 1">
+            <q-btn color="secondary" icon-right="event" label="Reserve"  @click="reserve = true" />
+              <q-dialog v-model="reserve" persistent>
+                <q-card style="min-width: 350px">
+                  <q-card-section>
+                    <div class="text-h6">Reservation Request</div>
+                    <div class = "text-h7">{{book.title}}</div>
+                  </q-card-section>
+                  <q-card-section>
+                    <div class="text-h7">Fill out the following form to submit your request</div>
+                  </q-card-section>
+                  <q-card-section class="q-pt-none">
+                    <q-input filled v-model="forename" label="Forename*" stack-label :dense="dense" :rules="[val => !!val || 'Field is required']" />
+                  </q-card-section>
+                   <q-card-section class="q-pt-none">
+                    <q-input filled v-model="surname" label="Surname*" stack-label :dense="dense" :rules="[val => !!val || 'Field is required']" />
+                  </q-card-section>
+                   <q-card-section class="q-pt-none">
+                    <q-input filled v-model="studentNumber" label="Student Number*" stack-label :dense="dense" :rules="[val => !!val || 'Field is required']" />
+                  </q-card-section>
+                <q-card-section>
+                <div class = "text-h6">Preferred Collection Method</div>
+                <div class="q-gutter-sm">
+                  <q-radio dense v-model="collection" val="Pick up" label="Pick up" />
+                  <q-radio dense v-model="collection" val="Delivery" label="Delivery" />
+                </div>
+                </q-card-section>
+                  <q-card-actions align="right" class="text-primary">
+                    <q-btn flat label="Cancel" v-close-popup />
+                    <q-btn flat label="Next" v-close-popup type="next"/>
+                  </q-card-actions>
+                </q-card>
+              </q-dialog>
+          </div>
+          <div v-else>
+            <q-btn color="red" icon-right="send"  label="Notify" @click="notify = true"/>
+            <q-dialog v-model="notify" persistent>
+                <q-card style="min-width: 350px">
+                  <q-card-section>
+                    <div class="text-h6">{{book.title}} is not currently in the library. Enter your email to be notified when it is available:</div>
+                  </q-card-section>
+
+                  <q-card-section class="q-pt-none">
+                    <q-input dense v-model="email" autofocus @keyup.enter="prompt = false" />
+                  </q-card-section>
+
+                  <q-card-actions align="right" class="text-primary">
+                    <q-btn flat label="Cancel" v-close-popup />
+                    <q-btn flat label="Submit" v-close-popup />
+                  </q-card-actions>
+                </q-card>
+              </q-dialog>
+          </div>
         </q-card-actions>
-        
       </q-card>
     </div>
   </q-page>
@@ -47,13 +88,21 @@ export default {
   name: 'PageIndex',
   data(){
     return {
+      notify: false,
+      reserve: false,
+      //validDetails: false,
+      email: '',
+      forename: '',
+      surname: '',
+      collection: '',
+      dense: false,
+      studentNumber: '',
       service: new BackendService(),
       allBooks: [],
       books: [],
       states_dictionary:{ 
         1:"AVAILABLE", 
-        2:"UNAVAILABLE", 
-        3:"QUARANTINED"
+        2:"UNAVAILABLE"
       }
     };
   },
@@ -72,6 +121,29 @@ export default {
           alert(err.message);
           }
       })
+  },
+  methods: {
+    onNext () {
+      this.$refs.forename.validate()
+      this.$refs.email.validate()
+
+      if (this.$refs.name.hasError || this.$refs.age.hasError) {
+        this.formHasError = true
+      }
+      else if (this.accept !== true) {
+        this.$q.notify({
+          color: 'negative',
+          message: 'You need to accept the license and terms first'
+        })
+      }
+      else {
+        this.$q.notify({
+          icon: 'done',
+          color: 'positive',
+          message: 'Submitted'
+        })
+      }
+    }
   }
 }
 </script>
