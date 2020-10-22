9579c47c88ff93116f84ad2030e35c24b8cf4aac
sophie-norman <s.norman2@newcastle.ac.uk>
2020-10-22 02:27:57 +0100

    form is fixed

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="flex flex-center">
     <div class="q-pa-md row items-start q-gutter-md">
-      <q-card class="my-card card-size" v-for="book in allBooks" :key="book">
+      <q-card class="my-card card-size" v-for="book in allBooks" :key="book._id">
         <q-card-section>
           <q-img :src="book.cover" >
             <p class="absolute-top-right defau-label" :class="{ 'avai-label' : book.status == 1, 'notavai-label' : book.status == 2 }">
@@ -28,60 +28,37 @@
 
         <q-card-actions>
           <div v-if="book.status == 1">
-            <q-btn color="secondary" icon-right="event" label="Reserve"  @click="reserve = true" />
-            <q-dialog v-model="reserve" persistent>
-              <q-card style="min-width: 350px">
-                <q-card-section>
-                  <div class="text-h6">Reservation Request</div>
-                  <div class = "text-h7">{{book.title}}</div>
-                </q-card-section>
-                <q-card-section>
-                  <div class="text-h7">Fill out the following form to submit your request</div>
-                </q-card-section>
-                <q-card-section class="q-pt-none">
-                  <q-input filled v-model="forename" label="Forename*" stack-label :dense="dense" :rules="[val => !!val || 'Field is required']" />
-                </q-card-section>
-                  <q-card-section class="q-pt-none">
-                  <q-input filled v-model="surname" label="Surname*" stack-label :dense="dense" :rules="[val => !!val || 'Field is required']" />
-                </q-card-section>
-                  <q-card-section class="q-pt-none">
-                  <q-input filled v-model="studentNumber" label="Student Number*" stack-label :dense="dense" :rules="[val => !!val || 'Field is required']" />
-                </q-card-section>
-              <q-card-section>
-              <div class = "text-h6">Preferred Collection Method</div>
-              <div class="q-gutter-sm">
-                <q-radio dense v-model="collection" val="Pick up" label="Pick up" />
-                <q-radio dense v-model="collection" val="Delivery" label="Delivery" />
-              </div>
-              </q-card-section>
-                <q-card-actions align="right" class="text-primary">
-                  <q-btn flat label="Cancel" v-close-popup />
-                  <q-btn flat label="Next" v-close-popup type="next"/>
-                </q-card-actions>
-              </q-card>
-            </q-dialog>
-          </div>
-          <div v-else>
-            <q-btn color="red" icon-right="send"  label="Notify" @click="notify = true"/>
-            <q-dialog v-model="notify" persistent>
-                <q-card style="min-width: 350px">
-                  <q-card-section>
-                    <div class="text-h6">{{book.title}} is not currently in the library. Enter your email to be notified when it is available:</div>
-                  </q-card-section>
-
-                  <q-card-section class="q-pt-none">
-                    <q-input dense v-model="email" autofocus @keyup.enter="prompt = false" />
-                  </q-card-section>
-
-                  <q-card-actions align="right" class="text-primary">
-                    <q-btn flat label="Cancel" v-close-popup />
-                    <q-btn flat label="Submit" v-close-popup />
-                  </q-card-actions>
-                </q-card>
-              </q-dialog>
+            <q-btn color="secondary" icon-right="event" label="Reserve" @click="openForm($event, book)" />
           </div>
         </q-card-actions>
       </q-card>
+
+      <q-dialog v-model="openDialog" persistent>
+        <q-card style="min-width: 350px" class="q-pa-md q-gutter-md">
+          <q-card-section style="margin-top:5px; padding:5px 10px">
+            <div class="text-h4">Reservation Request</div>
+            <div class = "text-h5">{{selectedBook.title}}</div>
+          </q-card-section>
+          <q-card-section style="margin-top:5px; padding:5px 10px">
+            <div class="text-h5">Enter Your Details</div>
+          </q-card-section>
+          <q-card-section style="margin-top:5px; padding:5px 10px">
+            <q-form class="q-gutter-sm">
+              <q-input filled 
+                ref="studentNumber" v-model="formDetails.studentNumber"
+                label="Student Number*" stack-label :dense="dense"
+                :rules="[val => !!val || 'Field is required']" lazy-rules />
+              <p class="text-h6">Preferred Delivery Method</p>
+              <q-radio dense v-model="formDetails.collectionMethod" val="Pick up" label="Pick up" />
+              <q-radio dense v-model="formDetails.collectionMethod" val="Delivery" label="Delivery" />
+            </q-form>
+          </q-card-section>
+          <q-card-actions align="right" class="text-primary">
+            <q-btn flat label="Cancel" v-close-popup />
+            <q-btn flat label="Next" @click="onNext" type="next"/>
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
     </div>
   </q-page>
 </template>
@@ -92,22 +69,23 @@ export default {
   name: 'PageIndex',
   data(){
     return {
-      notify: false,
-      reserve: false,
-      //validDetails: false,
-      email: '',
-      forename: '',
-      surname: '',
-      collection: '',
+      formDetails: {
+        email: '',
+        studentNumber: '',
+        collectionMethod: 0,
+        error: false
+      },
       dense: false,
-      studentNumber: '',
       service: new BackendService(),
       allBooks: [],
       books: [],
       states_dictionary:{ 
         1:"AVAILABLE", 
         2:"UNAVAILABLE"
-      }
+      },
+      selectedBook: {},
+      openDialog: false,
+      accept: false
     };
   },
   created() {
@@ -128,25 +106,27 @@ export default {
   },
   methods: {
     onNext () {
-      this.$refs.forename.validate()
-      this.$refs.email.validate()
+      this.$refs.studentNumber.validate();
 
-      if (this.$refs.name.hasError || this.$refs.age.hasError) {
-        this.formHasError = true
-      }
-      else if (this.accept !== true) {
+      if (this.$refs.studentNumber.hasError) {
+        this.formDetails.error = true;
+      } else if (this.accept !== true) {
         this.$q.notify({
           color: 'negative',
           message: 'You need to accept the license and terms first'
         })
-      }
-      else {
+      } else {
         this.$q.notify({
           icon: 'done',
           color: 'positive',
           message: 'Submitted'
         })
       }
+    },
+    openForm(ev, book) {
+      console.log(book);
+      this.selectedBook = book;
+      this.openDialog = true;
     }
   }
 }
